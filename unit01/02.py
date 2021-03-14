b70f8db6d45f9d6d10d520fc9f3115d388bb45bc
sikyungkim <sikoung0129@naver.com>
2021-03-14 23:35:21 +0900

    test code

--- unit01/02.py
+++ unit01/02.py
@@ -16,9 +16,8 @@ conditions = kiwoom.GetConditionNameList()
 condition_index = conditions[0][0]
 condition_name = conditions[0][1]
 codes = kiwoom.SendCondition("0101", condition_name, condition_index, 0)
-print(codes)
 
-# 삼성전자, 10주, 시장가주문 매수
+# 조건식 종목, 시장가주문 매수
 for code in codes:
     kiwoom.SendOrder("시장가매수", "0101", stock_account, 1, code, 10, 0, "03", "")
     time.sleep(0.2)
