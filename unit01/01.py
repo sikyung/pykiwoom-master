95d5bc601406d35a0411be2c565b622590cde91f
sikyungkim <sikoung0129@naver.com>
2021-03-14 23:29:40 +0900

    test code

--- unit01/01.py
+++ unit01/01.py
@@ -1,5 +1,19 @@
 from pykiwoom.kiwoom import *
 
-kiwoom = Kiwoom()
-kiwoom.CommConnect(block=True)
-print("블록킹 로그인 완료")
+kiwoom = Kiwoom(login=True)
+
+account_list = kiwoom.GetLoginInfo("ACCNO")
+account =  account_list[0]
+
+#opw00001
+df = kiwoom.block_request("opw00001",
+                          계좌번호=account,
+                          비밀번호="",
+                          비밀번호입력매체구분="00",
+                          조회구분=1,
+                          output="예수금상세현황",
+                          next=0)
+
+print(df)
+주문가능금액 = int(df['주문가능금액'][0])
+print(주문가능금액)
---
+++ unit01/02.py
@@ -0,0 +1,28 @@
+from pykiwoom.kiwoom import *
+import time
+
+kiwoom = Kiwoom()
+kiwoom.CommConnect(block=True)
+
+# 주식계좌
+accounts = kiwoom.GetLoginInfo("ACCNO")
+stock_account = accounts[0]
+
+# 조건식 load
+kiwoom.GetConditionLoad()
+conditions = kiwoom.GetConditionNameList()
+
+# 0번 조건식에 해당하는 종목 리스트 출력
+condition_index = conditions[0][0]
+condition_name = conditions[0][1]
+codes = kiwoom.SendCondition("0101", condition_name, condition_index, 0)
+print(codes)
+
+# 삼성전자, 10주, 시장가주문 매수
+for code in codes:
+    kiwoom.SendOrder("시장가매수", "0101", stock_account, 1, code, 10, 0, "03", "")
+    time.sleep(0.2)
+    print(code, "종목 시장가 매수 완료")
+
+
+
