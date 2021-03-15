d5a6808cb492b6f86b619ae5ea5c6460399c59b0
sikyung <sikoung0129@naver.com>
2021-03-15 13:23:18 +0900

    Real-time buyout

--- unit01/02.py
+++ unit01/02.py
@@ -8,20 +8,26 @@ kiwoom.CommConnect(block=True)
 accounts = kiwoom.GetLoginInfo("ACCNO")
 stock_account = accounts[0]
 
+# 매수 Test
+# kiwoom.SendOrder("시장가매수", "0101", stock_account, 1, "005930", 1, 0, "03", "")
+
 # 조건식 load
 kiwoom.GetConditionLoad()
 conditions = kiwoom.GetConditionNameList()
+print(conditions)
 
 # 0번 조건식에 해당하는 종목 리스트 출력
 condition_index = conditions[0][0]
 condition_name = conditions[0][1]
+print(condition_index, condition_name)
 codes = kiwoom.SendCondition("0101", condition_name, condition_index, 0)
+print(codes)
 
 # 조건식 종목, 시장가주문 매수
 for code in codes:
-    kiwoom.SendOrder("시장가매수", "0101", stock_account, 1, code, 10, 0, "03", "")
+    kiwoom.SendOrder("시장가매수", "0101", stock_account, 1, code, 1, 0, "03", "")
     time.sleep(0.2)
-    print(code, "종목 시장가 매수 완료")
+    print(code, "매수 완료")
 
 
 
